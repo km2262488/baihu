f90941f927f77798f3e0a40aca674b3f464da679
km2262488 <km2262488@gmail.com>
2025-09-13 22:54:50 +0800

    ipfinder.py

--- ipfinder.py
+++ ipfinder.py
@@ -1,6 +1,6 @@
 import socket as s
 print('eg : python3 ipfinder.py')
-print('And type url of any site without http or https eg:www.google.com')
+print('And type url of a site without http or https eg:www.google.com')
 try:
     host = input('Enter the url of the website : ')
     print(f'IP of {host} is {s.gethostbyname(host)}')
